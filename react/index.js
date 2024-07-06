8ab05130716c1b00e7db27811a22ea359aa647ba
irensula <irinawebdev@gmail.com>
2024-07-06 21:37:39 +0300

    Add files via upload

---
+++ react/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import reactDom from "react-dom";
+
+reactDom.render(
+  <div>
+    <h1>My Favourite Food</h1>
+    <ul>
+      <li>Pork</li>
+      <li>Fish</li>
+      <li>Cake</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
