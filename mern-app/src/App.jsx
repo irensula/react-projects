229f39129742d1c3356a2058a86a49316f123034
irina <irinawebdev@gmail.com>
2025-01-07 17:30:49 +0200

    mern-app

---
+++ mern-app/src/App.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { 
+  HomeLayout, 
+  Landing, 
+  Register, 
+  Login, 
+  DashboardLayout,
+  Error
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <HomeLayout />,
+  },
+  {
+    path:'/register',
+    element: <Register />,
+  },
+  {
+    path:'/login',
+    element: <Login />,
+  },
+  {
+    path:'/dashboard',
+    element: <DashboardLayout />,
+  },
+  {
+    path:'/error',
+    element: <Error />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+}
+
+export default App
---
+++ mern-app/src/pages/EditJob.jsx
@@ -0,0 +1,9 @@
+const EditJob = () => {
+    return (
+      <div>
+        <h1>Edit Job</h1>
+      </div>
+    )
+  }
+  
+  export default EditJob;
